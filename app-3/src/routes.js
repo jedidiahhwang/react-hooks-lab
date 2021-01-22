0fecd669ff1ba5ae777ea1449a12dd081d171393
Jedidiah <jedidiah.hwang@gmail.com>
2021-01-22 07:57:28 -0700

    final submission

---
+++ app-3/src/routes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Switch } from "react-router-dom";
+import {Route} from "react-router-dom";
+import Pokemon from "./Pokemon";
+import PokemonList from "./PokemonList";
+
+export default (
+    <Switch>
+        <Route exact path="/" component={PokemonList}/>
+        <Route path="/pokemon/:name" component={Pokemon}/>
+    </Switch>
+)
